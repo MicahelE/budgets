e45afc7a256fc229edb7688a46990a7b05b4643d
Michael Etokakpan <imohgenius@yahoo.com>
2022-11-24 13:37:15 +0100

    fresh

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 
 function myFunction() {
     let income = document.getElementById("income").value;
-    // let age = document.getElementById("age");
-    // let gender = document.getElementById("gender");
-    // let status = document.getElementById("status");
-    // let dependents = document.getElementById("dependents");
-    // let persons = document.getElementById("persons");
+    let age = document.getElementById("age").value;
+    let gender = document.getElementById("gender").value;
+    let status = document.getElementById("status").value;
+    let dependents = document.getElementById("dependents").value;
+    let person = document.getElementById("person").value;
     let span = document.getElementById("output");
     if (income == "") {
         span.innerText="No input was given"
     }
 
-    if (income>= 1) {
-        span.innerText="This is good"
+    if (income>= 1 && age <=18 ) {
+        let expense = income/2;
+    span.innerText= ` Spend  ₦${expense}  save  ₦${expense}` ;
     }
+    else if (income>= 1 && gender =='Male') {
+        let expense = income* 0.6;
+        let remain = income-saving;
+        span.innerText= ` Spend  ₦${expense}  save  ₦${remain}` ;
+    }
+    else if (income>= 1 && gender =='Female') {
+        let expense = income* 0.4;
+        let remain = income-saving;
+        span.innerText= ` Spend  ₦${expense}  save  ₦${remain}` ;
+    }
+
+    else if (income>= 1 && dependents >= 1 ) {
+        let expense = income -(dependents* person);
+        let remain = income-saving;
+        span.innerText= ` Spend  ₦${expense}  save  ₦${remain}` ;
+    }
+
+   
   }

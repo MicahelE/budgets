afe39956e71e1a2a2f7664ba27da40bbd4486003
Michael Etokakpan <imohgenius@yahoo.com>
2022-11-24 17:50:35 +0100

    logic

--- script.js
+++ script.js
@@ -1,36 +1,103 @@
 
 function myFunction() {
-    let income = document.getElementById("income").value;
-    let age = document.getElementById("age").value;
-    let gender = document.getElementById("gender").value;
-    let status = document.getElementById("status").value;
-    let dependents = document.getElementById("dependents").value;
-    let person = document.getElementById("person").value;
+    let income = parseInt(document.getElementById("income").value);
+    let age = parseInt(document.getElementById("age").value);
+    let genders = document.getElementsByName('gender');
+let gender_value;
+for(let i = 0; i < genders.length; i++){
+    if(genders[i].checked){
+        gender_value = genders[i].value;
+    }
+}
+
+    let statuses = document.getElementsByName('status');
+    let status_value;
+    for(let i = 0; i < statuses.length; i++){
+        if(statuses[i].checked){
+            status_value = statuses[i].value;
+        }
+    }
+    let dependents = parseInt(document.getElementById("dependents").value);
+    let person = parseInt(document.getElementById("person").value);
     let span = document.getElementById("output");
     if (income == "") {
         span.innerText="No input was given"
     }
 
-    if (income>= 1 && age <=18 ) {
-        let expense = income/2;
-    span.innerText= ` Spend  ₦${expense}  save  ₦${expense}` ;
+    if (income== "" || age =="" || gender_value =="" || status_value =="" || dependents =="" || person =="" ) {
+        alert("Fill all the fields"); 
     }
-    else if (income>= 1 && gender =='Male') {
-        let expense = income* 0.6;
-        let remain = income-saving;
-        span.innerText= ` Spend  ₦${expense}  save  ₦${remain}` ;
+    else if (gender_value =="male" && status_value =="single" ) {
+        let expense = income -(dependents* person);
+        let save= income -expense;
+        if (age<=30 && income < 20000) {
+            span.innerText= `Look seriously into saving and upskilling You have to save  ₦${expense}  and spend  ₦${save}` ;  
+        }
+        else if (age>=30 && income < 20000) {
+            span.innerText= `Look seriously into Stable source of income You have to save  ₦${expense}  and spend  ₦${save}` ;  
+        }
+        else if (age>=30 && income > 20000) {
+            span.innerText= `Go and marry  ₦${expense}  and spend  ₦${save}` ;  
+        }
+        else if (age<=30 && income > 20000) {
+            span.innerText= `Looking good Try and do investments You have to save  ₦${expense}  and spend  ₦${save}` ;  
+        }
     }
-    else if (income>= 1 && gender =='Female') {
-        let expense = income* 0.4;
-        let remain = income-saving;
-        span.innerText= ` Spend  ₦${expense}  save  ₦${remain}` ;
+    else if (gender_value =="female" && status_value =="single" ) {
+        let expense = income -(dependents* person);
+        let save= income -expense;
+        if (age<=30 && income < 20000) {
+            span.innerText= `Look seriously into saving and upskilling You have to save  ₦${expense}  and spend  ₦${save}` ;  
+        }
+        else if (age>=30 && income < 20000) {
+            span.innerText= `Look seriously into Stable source of income You have to save  ₦${expense}  and spend  ₦${save}` ;  
+        }
+        else if (age>=30 && income > 20000) {
+            span.innerText= `Go and marry You have to save  ₦${expense}  and spend  ₦${save}` ;  
+        }
+        else if (age<=30 && income > 20000) {
+            span.innerText= `Looking good Try and do investments You have to save  ₦${expense}  and spend  ₦${save}` ;  
+        }
+    }
+    else if (gender_value =="male" && status_value =="married" ) {
+        let expense = income -(dependents* person);
+        let save= income -expense;
+        if (age<=30 && income < 20000) {
+            span.innerText= `Look seriously into saving and upskilling, You have to save ₦${expense}  and spend ${save}` ;  
+        }
+        else if (age>=30 && income < 20000) {
+            span.innerText= `Look seriously into Stable source of income You have to save ₦${expense}  and spend ${save}` ;  
+        }
+        else if (age>=30 && income > 20000) {
+            span.innerText= `Go and marry  You have to save ₦${expense} and spend  ₦${save}` ;  
+        }
+        else if (age<=30 && income > 20000) {
+            span.innerText= `Looking good Try and do investments, You have to save ₦${expense} and spend  ₦${save}` ;  
+        }
     }
 
-    else if (income>= 1 && dependents >= 1 ) {
+    else if (gender_value =="female" && status_value =="married" ) {
         let expense = income -(dependents* person);
-        let remain = income-saving;
-        span.innerText= ` Spend  ₦${expense}  save  ₦${remain}` ;
+        let save= income -expense;
+        if (age<=30 && income < 20000) {
+            span.innerText= `Look seriously into saving and upskilling,  You have to save ₦${expense} and spend  ₦${save}` ;  
+        }
+        else if (age>=30 && income < 20000) {
+            span.innerText= `Look seriously into Stable source of income,  You have to save ₦${expense} and spend  ₦${save}` ;  
+        }
+        else if (age>=30 && income > 20000) {
+            span.innerText= `Go and marry  You have to save ₦${expense} and spend  ₦${save}` ;  
+        }
+        else if (age<=30 && income > 20000) {
+            span.innerText= `Looking good Try and do investments  You have to save ₦${expense} and spend  ₦${save}` ;  
+        }
     }
+
+    // else if (income>= 1 && dependents >= 1 ) {
+    //     let expense = income -(dependents* person);
+    //     let remain = income-saving;
+    //     span.innerText= ` Spend  ₦${expense}  save  ₦${remain}` ;
+    // }
 
    
   }
